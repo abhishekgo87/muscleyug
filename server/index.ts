725a12b54be8c0aaa76c983efaeb91661b49566e
abhishekgo87 <abhishekgoswami8791@gmail.com>
2026-07-20 18:43:03 +0530

    initialize Express server with auth routes and grant database permissions to users table

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { supabase } from './supabaseClient';
+import authRoutes from './routes/authRoutes';
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Muscleyug backend is running!');
